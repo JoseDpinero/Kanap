cac48b2c9d4322cfbacade07fb97a84faf953ec5
Jose Pinero <shine.shinete@gmail.com>
2022-12-06 17:21:31 +0100

    ajout et suppression des produits du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,102 +1,157 @@
 let Products = async () => fetch('http://localhost:3000/api/products')
     .then(res => res.json())
-    .then(function (allProducts) {
+    .then(function (data) {
 
-        return allProducts
+        return data
     });
 
-Products();
-
-class cartProduct {
-
-    constructor(_id, color, quantity) {
-
-        this._id = _id;
-        this.color = color;
-        this.quantity = quantity;
-
-    }
-    async checkProduct(element) {
-
-        let allProducts = await Products();
-     let elementChecked = [];
-        allProducts.forEach(elt => {
-
-            if (element._id == elt['_id']) {
-
-                elementChecked = elt;
-            }
-
-        });
-        return elementChecked;
-    }
-
-    async createItem(element) {
-
-        let elt = await this.checkProduct(element);
-
-        const cart__item = document.getElementById('cart__items');
-        let article = document.createElement('article');
-
-        cart__item.appendChild(article);
-
-        article.classList.add('cart__item');
-        article.setAttribute('data-id', element._id);
-        article.setAttribute('data-color', element.color);
-
-        let image = document.createElement('div');
-        image.classList.add('cart__item__img');
-        image.innerHTML = `<img src="`
-            + elt.imageUrl + `" alt="`
-            + elt.altTxt + `">`;
-
-        article.appendChild(image);
-
-        let content = document.createElement('div');
-        content.classList.add('cart__item__content');
-        let description = document.createElement('div');
-        description.classList.add("cart__item__content__description");
-
-        let name = document.createElement('h2')
-        name.innerText = elt.name;
-        description.appendChild(name);
-
-        let color = document.createElement('p');
-        color.innerHTML = element.color;
-        description.appendChild(color);
-
-        let price = document.createElement('p');
-        price.innerHTML = elt.price + `,00 €`;
-        description.appendChild(price);
-
-        content.appendChild(description);
-
-        article.appendChild(content);
-
-        let contentSetting = document.createElement('div');
-        contentSetting.classList.add('cart__item__content__settings');
-        contentSetting.innerHTML = `<div class="cart__item__content__settings__quantity">
-        <p>Qté : </p><input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="`+ element.quantity + `">
-           </div>`;
-
-        article.appendChild(contentSetting);
-
-        let del = document.createElement('div');
-        del.classList.add('cart__item__content__settings__delete');
-        del.innerHTML = `<p class="deletItem"> Supprimer</p>`;
-
-        article.appendChild(del);
-               
-    }    
-    
+function getStorage() {
+    return JSON.parse(localStorage.getItem('product'))
 }
 
-recupStorage = JSON.parse(localStorage.getItem('product'));
+function productsPrice(products) {
 
-recupStorage.forEach(element => {
+    let totalPrice = 0;
+    let totalQuantity = 0;
 
-    let currentProduct = new cartProduct(element._id, element.color, element.quantity);
+    for (let i = 0; i < products.length; i++) {
 
-    currentProduct.createItem(element);
+        totalPrice += products[i].quantity * products[i].price;
+        totalQuantity += products[i].quantity;
+    }
 
-});
+    document.getElementById('totalPrice').innerText = totalPrice;
+    document.getElementById('totalQuantity').innerText = totalQuantity;
+
+}
+
+async function checkProduct(element) {
+
+    let allProducts = await Products();
+    let elementChecked = [];
+    allProducts.forEach(elt => {
+
+        if (element._id == elt['_id']) {
+
+            elementChecked = elt;
+        }
+
+    });
+    return elementChecked;
+}
+
+async function createArticleItem(element) {
+
+    let elt = await this.checkProduct(element);
+
+    const cart__item = document.getElementById('cart__items');
+    let article = document.createElement('article');
+
+    cart__item.appendChild(article);
+
+    article.classList.add('cart__item');
+    article.setAttribute('data-id', element._id);
+    article.setAttribute('data-color', element.color);
+
+    let image = document.createElement('div');
+    image.classList.add('cart__item__img');
+    image.innerHTML = `<img src="`
+        + elt.imageUrl + `" alt="`
+        + elt.altTxt + `">`;
+
+    article.appendChild(image);
+
+    let content = document.createElement('div');
+    content.classList.add('cart__item__content');
+    let description = document.createElement('div');
+    description.classList.add("cart__item__content__description");
+
+    let name = document.createElement('h2')
+    name.innerText = elt.name;
+    description.appendChild(name);
+
+    let color = document.createElement('p');
+    color.innerHTML = element.color;
+    description.appendChild(color);
+
+    let price = document.createElement('p');
+    price.innerHTML = elt.price + `,00 €`;
+    description.appendChild(price);
+
+    content.appendChild(description);
+
+    article.appendChild(content);
+
+    let contentSetting = document.createElement('div');
+    contentSetting.classList.add('cart__item__content__settings');
+    contentSetting.innerHTML = `<div class="cart__item__content__settings__quantity">
+    <p>Qté : </p><input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="`+ element.quantity + `">
+      </div>`;
+
+    article.appendChild(contentSetting);
+
+    let del = document.createElement('div');
+    del.classList.add('cart__item__content__settings__delete');
+    del.innerHTML = `<p class="deleteItem"> Supprimer</p>`;
+
+    article.appendChild(del);
+
+
+
+}
+
+function createCartItems() {
+
+    let currentStorage = getStorage();
+
+    currentStorage.forEach(element => { createArticleItem(element); });
+    productsPrice(currentStorage);
+
+}
+
+
+async function modifyOrder() {
+    const deleteBtn = document.querySelectorAll('.deleteItem');
+    const inputNumber = document.querySelectorAll('.itemQuantity');
+    const article = document.querySelectorAll('.cart__item');
+    const section = document.getElementById('cart__items');
+
+    for (let i = 0; i < deleteBtn.length; i++) {
+
+        deleteBtn[i].addEventListener('click', function (event) {
+
+            let articlesSelected = getStorage();
+            const targetElement = event.target.closest('.cart__item').dataset;
+            let indexArticle = articlesSelected.findIndex((element) => element._id == targetElement.id && element.color == targetElement.color);
+
+            section.removeChild(article[i]);
+            articlesSelected.splice(indexArticle, 1);
+
+            localStorage.setItem('product', JSON.stringify(articlesSelected));
+            productsPrice(articlesSelected);
+
+        })
+    }
+
+
+    for (let i = 0; i < inputNumber.length; i++) {
+
+        inputNumber[i].addEventListener('change', function (event) {
+
+            let articlesSelected = getStorage();
+
+            const targetElement = event.target.closest('.cart__item').dataset;
+            let indexArticle = articlesSelected.findIndex((element) => element._id == targetElement.id && element.color == targetElement.color);
+
+            articlesSelected[indexArticle].quantity = +event.target.value;
+
+            localStorage.setItem('product', JSON.stringify(articlesSelected));
+            productsPrice(articlesSelected);
+
+        })
+    }
+}
+
+Products()
+    .then(createCartItems())
+    .then(setTimeout(modifyOrder, 500));
